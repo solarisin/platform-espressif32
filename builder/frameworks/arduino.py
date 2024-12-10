a718463dd3c74201d928e495007da591815d49cb
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-12-10 18:17:37 +0100

    fix not defined var

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -34,7 +34,8 @@ build_core = board.get("build.core", "").lower()
 SConscript("_embed_files.py", exports="env")
 
 if "espidf" not in env.subst("$PIOFRAMEWORK"):
-    if os.path.exists(join(FRAMEWORK_DIR, "tools", "platformio-build.py")):
+    if os.path.exists(join(DefaultEnvironment().PioPlatform().get_package_dir(
+            "framework-arduinoespressif32"), "tools", "platformio-build.py")):
         PIO_BUILD = "platformio-build.py"
     else:
         PIO_BUILD = "pioarduino-build.py"
