f9c69119302bd8b1ddbb30f16d94f7e1f40f2127
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-02-22 12:59:48 +0100

    Update platform.py

--- platform.py
+++ platform.py
@@ -91,11 +91,6 @@ class Espressif32Platform(PlatformBase):
         if "arduino" in frameworks:
             self.packages["framework-arduinoespressif32"]["optional"] = False
             self.packages["framework-arduinoespressif32-libs"]["optional"] = False
-            # use latest espressif Arduino libs
-            URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/release/v3.1.x/package/package_esp32_index.template.json"
-            packjdata = requests.get(URL).json()
-            dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
-            self.packages["framework-arduinoespressif32-libs"]["version"] = dyn_lib_url
 
         if variables.get("custom_sdkconfig") is not None or len(str(board_sdkconfig)) > 3:
             frameworks.append("espidf")
