87acbf405d451c3dcfc558aec7f0a454f2cd512b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-03-09 21:24:18 +0100

    Update and rename lp_core_pulse_counter_example_main.c to lp_core_gpio_wake_up_example_main.c

--- examples/espidf-ulp-lp/src/lp_core_gpio_wake_up_example_main.c
+++ examples/espidf-ulp-lp/src/lp_core_gpio_wake_up_example_main.c
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: Unlicense OR CC0-1.0
  */
-/* LP core gpio example
+/* ULP LP-Core GPIO wake-up example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -13,21 +13,29 @@
 */
 
 #include <stdio.h>
-#include <inttypes.h>
 #include "esp_sleep.h"
-#include "driver/gpio.h"
 #include "driver/rtc_io.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "ulp_lp_core.h"
 #include "ulp_main.h"
-#include "lp_core_uart.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define WAKEUP_PIN 2
 
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
 
-
 static void init_ulp_program(void);
+
+static void wakeup_gpio_init(void)
+{
+    /* Configure the button GPIO as input, enable wakeup */
+    rtc_gpio_init(WAKEUP_PIN);
+    rtc_gpio_set_direction(WAKEUP_PIN, RTC_GPIO_MODE_INPUT_ONLY);
+    rtc_gpio_pulldown_dis(WAKEUP_PIN);
+    rtc_gpio_pullup_en(WAKEUP_PIN);
+    rtc_gpio_wakeup_enable(WAKEUP_PIN, GPIO_INTR_NEGEDGE);
+}
 
 void app_main(void)
 {
@@ -38,29 +46,26 @@ void app_main(void)
     */
     vTaskDelay(pdMS_TO_TICKS(1000));
 
-    /* Initialize selected GPIO as RTC IO, enable input/output, disable pullup and pulldown */
-    printf("Using pin %d as pulse counter input\n", CONFIG_EXAMPLE_PULSE_COUNT_PIN);
-    rtc_gpio_init(CONFIG_EXAMPLE_PULSE_COUNT_PIN);
-    rtc_gpio_set_direction(CONFIG_EXAMPLE_PULSE_COUNT_PIN, RTC_GPIO_MODE_INPUT_OUTPUT);
-    rtc_gpio_pulldown_dis(CONFIG_EXAMPLE_PULSE_COUNT_PIN);
-    rtc_gpio_pullup_dis(CONFIG_EXAMPLE_PULSE_COUNT_PIN);
-
-    printf("ULP will wake up processor after every %d pulses\n", CONFIG_EXAMPLE_PULSE_COUNT_WAKEUP_LIMIT);
+    wakeup_gpio_init();
 
     esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
     /* not a wakeup from ULP, load the firmware */
     if (cause != ESP_SLEEP_WAKEUP_ULP) {
         printf("Not a ULP wakeup, initializing it! \n");
         init_ulp_program();
-    } else {
-        printf("ULP woke up the main CPU!\n");
-        printf("Pulse count: %"PRIu32"\n", ulp_pulse_count);
+    }
+
+    /* ULP read and detected a change in WAKEUP_PIN, prints */
+    if (cause == ESP_SLEEP_WAKEUP_ULP) {
+        printf("ULP woke up the main CPU! \n");
     }
 
     /* Go back to sleep, only the ULP will run */
-    printf("Entering in deep sleep\n\n");
+    printf("Entering deep sleep\n\n");
 
     /* Small delay to ensure the messages are printed */
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
     ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup());
 
     esp_deep_sleep_start();
@@ -68,16 +73,12 @@ void app_main(void)
 
 static void init_ulp_program(void)
 {
-    lp_core_uart_cfg_t uart_cfg = LP_CORE_UART_DEFAULT_CONFIG();
-
-    ESP_ERROR_CHECK(lp_core_uart_init(&uart_cfg));
-
     esp_err_t err = ulp_lp_core_load_binary(ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start));
     ESP_ERROR_CHECK(err);
 
     /* Start the program */
     ulp_lp_core_cfg_t cfg = {
-        .wakeup_source = ULP_LP_CORE_WAKEUP_SOURCE_HP_CPU,
+        .wakeup_source = ULP_LP_CORE_WAKEUP_SOURCE_LP_IO,
     };
 
     err = ulp_lp_core_run(&cfg);
