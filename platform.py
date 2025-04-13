6e848c3f780120264a1667686931b2cfc3d987b4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-13 20:27:58 +0200

    Use actual develop 5.4. libs

--- platform.py
+++ platform.py
@@ -46,10 +46,12 @@ class Espressif32Platform(PlatformBase):
         if "arduino" in frameworks:
             self.packages["framework-arduinoespressif32"]["optional"] = False
             self.packages["framework-arduinoespressif32-libs"]["optional"] = False
-            # use latest espressif Arduino libs
-            URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/master/package/package_esp32_index.template.json"
-            packjdata = requests.get(URL).json()
-            dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
+            # use matching espressif Arduino libs
+            #URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/master/package/package_esp32_index.template.json"
+            #packjdata = requests.get(URL).json()
+            #dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
+            # use newer libs as linked in package_esp32_index.template.json is too old
+            dyn_lib_url = "https://github.com/espressif/esp32-arduino-lib-builder/releases/download/idf-release_v5.4/esp32-arduino-libs-idf-release_v5.4-6897a7bf-v1.zip"
             self.packages["framework-arduinoespressif32-libs"]["version"] = dyn_lib_url
 
         if variables.get("custom_sdkconfig") is not None or len(str(board_sdkconfig)) > 3:
