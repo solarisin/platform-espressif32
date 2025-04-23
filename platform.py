d81abd0f45d60251f14154ed9afd1db89a93c484
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-04-23 19:23:14 +0200

    Use Arduino libs idf-release_v5.4-3ad36321

--- platform.py
+++ platform.py
@@ -51,7 +51,7 @@ class Espressif32Platform(PlatformBase):
             #packjdata = requests.get(URL).json()
             #dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
             # use newer libs as linked in package_esp32_index.template.json is too old
-            dyn_lib_url = "https://github.com/espressif/esp32-arduino-lib-builder/releases/download/idf-release_v5.4/esp32-arduino-libs-idf-release_v5.4-6897a7bf-v1.zip"
+            dyn_lib_url = "https://github.com/espressif/esp32-arduino-lib-builder/releases/download/idf-release_v5.4/esp32-arduino-libs-idf-release_v5.4-3ad36321-v1.zip"
             self.packages["framework-arduinoespressif32-libs"]["version"] = dyn_lib_url
 
         if variables.get("custom_sdkconfig") is not None or len(str(board_sdkconfig)) > 3:
