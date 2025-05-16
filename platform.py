a94b5b3d38364b42bb7fdf172bd1851129fdf4a1
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-05-16 18:31:11 +0200

    fix logic to use intermediate branch

--- platform.py
+++ platform.py
@@ -120,14 +120,16 @@ class Espressif32Platform(PlatformBase):
         if "arduino" in frameworks:
             self.packages["framework-arduinoespressif32"]["optional"] = False
             self.packages["framework-arduinoespressif32-libs"]["optional"] = False
-            try:
-                # use newer branch idf-release/v5.4 when existing
-                self.packages["framework-arduinoespressif32"]["version"] = "https://github.com/espressif/arduino-esp32/archive/refs/heads/idf-release/v5.4.zip"
-                URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/idf-release/v5.4/package/package_esp32_index.template.json"
-            except:
+            # use newer branch idf-release/v5.4 when existing
+            self.packages["framework-arduinoespressif32"]["version"] = "https://github.com/espressif/arduino-esp32/archive/refs/heads/idf-release/v5.4.zip"
+            URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/idf-release/v5.4/package/package_esp32_index.template.json"
+            req = requests.get(URL)
+            if req.status_code == 200:
+                packjdata = req.json()
+            else:
                 # use branch master
                 URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/master/package/package_esp32_index.template.json"
-            packjdata = requests.get(URL).json()
+                packjdata = requests.get(URL).json()
             dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
             self.packages["framework-arduinoespressif32-libs"]["version"] = dyn_lib_url
 
